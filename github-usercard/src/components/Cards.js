e404bf59d62eaaae89bdd4f4b682916507435f1d
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-06 15:01:14 -0700

    made my own user card

--- github-usercard/src/components/Cards.js
+++ github-usercard/src/components/Cards.js
@@ -1,71 +1,55 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    minWidth: 275
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 });
 
-export default function SimpleCard() {
+export default function SimpleCard(props) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-      {this.props.user.login} 
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {props.user.login}
         </Typography>
         <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          <img src={props.user.avatar_url} alt="avatar" />
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      s
     </Card>
   );
 }
 
-export default class Cards extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    console.log("isaiah", this.props.user);
-    return (
-      <div>
-      
-      </div>
-    );
-  }
-}
+// export default class Cards extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     console.log("isaiah", this.props.user);
+//     return (
+//       <div>
+
+//       </div>
+//     );
+//   }
+// }
